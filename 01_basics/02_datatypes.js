c8e9946f904aa7d34b27adb8ff8e56580f81bb03
palajeet23886-developer <138991544+palajeet23886-developer@users.noreply.github.com>
2023-09-11 16:42:58 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict;" // Treat all Js Code as new version
+
+// alert() // we are using nodejs, not browser
+
+// Types of datatypes (primitive datatypes)
+
+// BB SS NN U ( Boolean BigInt String Symbol Null Number Undefined)
+
+// obeject // (non Primitive datatypes )
+
+console.log(typeof null);  // output object
+console.log(typeof undefined); //output undefined
+
+let name = "Ajeet";
+let age = 21 ;
+console.log(typeof "name" );
