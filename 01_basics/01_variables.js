b2929b6426ebf39cc4a56f8b0715f77c5085bd59
palajeet23886-developer <138991544+palajeet23886-developer@users.noreply.github.com>
2023-09-11 16:43:14 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+console.log("hello World!!");
+const accountId = "767343";
+let accountEmail = "ajeet@gmail.com";
+var accountpassword = "14234";
+accountCity = "fatehpur";
+let accountState;
+
+
+ 
+// const accountId = "416435"; // not allowed in js
+// console.log( accountId);
+
+
+ accountEmail = "aje@gmail.com";
+ accountPassword = "174614";
+accountCity = "amauli";
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+/* prefer not use var
+because of issue in block scope and functional scope */
