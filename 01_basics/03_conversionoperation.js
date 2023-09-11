c45c633d3c05f92120b57fab3ea9903ddf6c89d1
palajeet23886-developer <138991544+palajeet23886-developer@users.noreply.github.com>
2023-09-11 18:10:17 +0000

    conversionoperation

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -21,9 +21,9 @@
 
 
 
-let isLoggedIn = 1
-let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true
 // "" => false
@@ -31,7 +31,38 @@ console.log(booleanIsLoggedIn);
 // "Ajeet" => true
 
 
-let somenumber = 33;
-let stringNumber = String(somenumber);
-console.log(stringNumber);
-console.log( typeof stringNumber);
+// let somenumber = 33;
+// let stringNumber = String(somenumber);
+// console.log(stringNumber);
+// console.log( typeof stringNumber);
+
+
+// 🔹🔹🔹🔹🔹🔹🔹🔹🔹🔹🔹🔹🔹🔹🔹Operations🔹🔹🔹🔹🔹🔹🔹🔹🔹🔹🔹🔹🔹🔹🔹🔹🔹
+
+let str1 = "hello";
+let str2 = " Ajeet";
+let str3 = str1 + str2;
+console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+console.log(true);   // output = true
+console.log(+true);  // output = 1
+console.log(+"");  // output = 0
+
+
+let gameCounter = 100;
+    // gameCounter++;
+    ++gameCounter;
+console.log(gameCounter );
+
+
+
+
+
+
+
+
