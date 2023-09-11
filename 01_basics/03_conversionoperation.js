04543844b58bfd32e8982dca5f7f304ff7351428
palajeet23886-developer <138991544+palajeet23886-developer@users.noreply.github.com>
2023-09-11 17:30:14 +0000

    conversionoperation

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,37 @@
+// let score = 33;
+// console.log(typeof score);
+//console.log(typeof (score));
+// let score = 33;
+// let valueInnumber = Number(score);
+
+// // output = number
+
+// let score = "33";
+// let valueInnumber = Number(score);
+
+// // output = string
+
+
+// let value = output
+
+// "33abc" => NaN
+// true => 1 false => 0
+// null => 0
+// undefined => NaN
+
+
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true
+// "" => false
+// 0 => false
+// "Ajeet" => true
+
+
+let somenumber = 33;
+let stringNumber = String(somenumber);
+console.log(stringNumber);
+console.log( typeof stringNumber);
