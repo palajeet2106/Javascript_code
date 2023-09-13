2e4113204f0c7f18726eb1244acca7f6e01f7565
palajeet23886-developer <138991544+palajeet23886-developer@users.noreply.github.com>
2023-09-13 16:56:17 +0000

    object part 3

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,30 @@
+// objects destructuring
+
+const course = {
+    coursename : "JS in Hindi",
+    price : "999",
+    courseInstrutor : "Ajeet Pal"
+}
+// course.courseInstructor
+// console.log(course);
+
+// const{courseInstrutor} = course    // find the value
+// console.log(courseInstrutor);
+
+// const {courseInstrutor:Instructor} = course    // find the value
+// console.log(Instructor);
+
+// const{coursename} = course
+// console.log(coursename);
+
+// const{price} = course
+// console.log(price);
+
+
+// JSON API
+
+{
+    "name" : "Ajeet Pal",
+    "coursename" : "JS In Hindi",
+    "price" : "free"
+}
