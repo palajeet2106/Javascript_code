ce05d2715dc374e42398e429558e3c98562c24c8
palajeet23886-developer <138991544+palajeet23886-developer@users.noreply.github.com>
2023-09-14 20:51:32 +0000

    break and continue statement

---
+++ 05_iterations/02_break_continue.js
@@ -0,0 +1,23 @@
+// for (let i = 0; i <=10; i++) {
+//     const element = i;
+//     console.log(element);
+    
+// }
+
+
+// for (let i = 0; i <=10; i++) {
+//    if (i == 5) {
+//     console.log(`Detected 5`);
+//     break
+//    }
+//     console.log(`value of is ${i}`);
+// }
+
+
+for (let i = 0; i <=10; i++) {
+   if (i == 5) {
+    console.log(`Detected 5`);
+    continue
+   }
+    console.log(`value of is ${i}`);
+}
