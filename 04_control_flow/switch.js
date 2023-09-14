184adfd42e1a8bc3fde65fd0745768a26117eaf0
palajeet23886-developer <138991544+palajeet23886-developer@users.noreply.github.com>
2023-09-15 00:00:53 +0530

    Create switch.js

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,101 @@
+// syntax of switch
+ 
+//  switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+// const month = 3;
+// switch (month) {
+//     case 1:
+//         console.log("january");
+//         break;
+//     case 2:
+//         console.log("feb");
+//         break;
+//     case 3:
+//         console.log("march");
+//         break;
+//     case 4:
+//         console.log("April");
+//         break;
+//     case 5:
+//         console.log("may");
+//         break;
+//     case 6:
+//         console.log("june");
+//         break;
+        
+
+//     default:
+//         console.log("Month is not found");
+//         break;
+// }                                          // output = march
+
+
+
+
+// const month = "may";
+// switch (month) {
+//     case "jan":
+//         console.log("january");
+//         break;
+//     case "feb":
+//         console.log("feb");
+//         break;
+//     case "mar":
+//         console.log("march");
+//         break;
+//     case "apr":
+//         console.log("April");
+//         break;
+//     case "may":
+//         console.log("may");
+//         break;
+//     case "june":
+//         console.log("june");
+//         break;
+        
+
+//     default:
+//         console.log("Month is not found");
+//         break;
+// }                                          // output = may
+
+
+
+
+
+const month = "august";
+switch (month) {
+    case "jan":
+        console.log("january");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "mar":
+        console.log("march");
+        break;
+    case "apr":
+        console.log("April");
+        break;
+    case "may":
+        console.log("may");
+        break;
+    case "june":
+        console.log("june");
+        break;
+        
+
+    default:
+        console.log("Month is not found");
+        break;
+}                                          // output = Month is not found
+
+
