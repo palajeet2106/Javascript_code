51ef6056098e9a3c2fbd3cd6a4edf460ff19e0a4
palajeet23886-developer <138991544+palajeet23886-developer@users.noreply.github.com>
2023-09-15 07:12:35 +0000

    for of loop

---
+++ 05_iterations/04_forofloop.js
@@ -0,0 +1,73 @@
+
+// for of loop
+
+// [ "","",""]
+// [{},{},{}]
+
+const arr = [1,2,3,4,5];
+
+for (const num of arr) {
+   //  console.log(num);
+}
+
+const greetings = "Hello World!";
+
+for (const greet of greetings) {
+ //  console.log(`Each char is ${greet}`);
+}
+
+
+
+// Maps
+
+// const map = new Map();
+// map.set('IN' , "India")
+// map.set('USA' , "United State Of America")
+// map.set('FR' , "France")
+
+// console.log(map);          // output = Map(3) {
+//                                             //     'IN' => 'India',
+//                                             //     'USA' => 'United State Of America',
+//                                             //     'FR' => 'France'
+//                                             // }
+
+
+
+const map = new Map();
+map.set('IN' , "India")
+map.set('USA' , "United State Of America")
+map.set('FR' , "France")
+map.set('IN' , "India")    // note = map unique value k liye jane jate hai
+
+//console.log(map);          // output = Map(3) {
+                                            //     'IN' => 'India',
+                                            //     'USA' => 'United State Of America',
+                                            //     'FR' => 'France'
+                                            // }
+
+
+for (const key of map) {
+   // console.log(key);
+}                       //  output = [ 'IN', 'India' ]
+                         //           [ 'USA', 'United State Of America' ]
+                          //          [ 'FR', 'France' ]
+
+
+
+for (const [key , value] of map) {
+   // console.log(key , ':-', value);
+}                                     // output = IN :- India
+                                      //              USA :- United State Of America
+                                      //              FR :- France
+
+
+const myObject = {
+    'game1': "NFS",
+    'game2' : "spiderman"
+}
+
+for (const [key , value] of myObject) {
+    console.log(key , ':-', value);
+}                                         //output = myObject is not iterable
+// Note =  object m for of loop iterable nhi hai
+
